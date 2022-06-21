ad435476f922fa93c4d1cddb9cf318a01be52787
Bisrat-Kassa <107492307+Bisrat-Kassa@users.noreply.github.com>
2022-06-21 14:57:29 -0700

    Project Initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num;
+string ans;
+do
+{
+    // int num;
+    Console.WriteLine("Enter a number you want to check");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num == 0)
+        Console.WriteLine("Number is neither odd nor even");
+    else if (num % 2 == 0)
+        Console.WriteLine("Number is  EVEN");
+    else
+    {
+        Console.WriteLine("Number is ODD");
+    }
+
+        Console.WriteLine("Do you want to continue? Enter Y/N ");
+        ans = Console.ReadLine();
+} while (ans == "Y" || ans == "y" || ans =="yes");
+
+Console.ReadKey();
