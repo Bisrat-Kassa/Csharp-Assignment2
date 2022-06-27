084d2e53a70bb0135bbc5ba076ec46e92371a4bd
Bisrat-Kassa <107492307+Bisrat-Kassa@users.noreply.github.com>
2022-06-27 09:56:26 -0700

    v3

---
+++ search/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.withdraw = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Account_no = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.First_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Last_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "first name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "last name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "balance";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "account";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "create account";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(303, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Deposit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // withdraw
+            // 
+            this.withdraw.Location = new System.Drawing.Point(494, 231);
+            this.withdraw.Name = "withdraw";
+            this.withdraw.Size = new System.Drawing.Size(123, 48);
+            this.withdraw.TabIndex = 6;
+            this.withdraw.Text = "withdraw";
+            this.withdraw.UseVisualStyleBackColor = true;
+            this.withdraw.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(684, 231);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 48);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "transfer";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(639, 34);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 41);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "search";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(301, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(301, 26);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(127, 125);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(301, 26);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "0.00";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(127, 170);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(301, 26);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "0.00";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Account_no,
+            this.First_name,
+            this.Last_name,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(108, 334);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(666, 301);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // Account_no
+            // 
+            this.Account_no.Frozen = true;
+            this.Account_no.HeaderText = "Account_no";
+            this.Account_no.MinimumWidth = 8;
+            this.Account_no.Name = "Account_no";
+            this.Account_no.Width = 150;
+            // 
+            // First_name
+            // 
+            this.First_name.Frozen = true;
+            this.First_name.HeaderText = "First_name";
+            this.First_name.MinimumWidth = 8;
+            this.First_name.Name = "First_name";
+            this.First_name.Width = 150;
+            // 
+            // Last_name
+            // 
+            this.Last_name.Frozen = true;
+            this.Last_name.HeaderText = "Last_name";
+            this.Last_name.MinimumWidth = 8;
+            this.Last_name.Name = "Last_name";
+            this.Last_name.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Balance";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 700);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.withdraw);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button withdraw;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Account_no;
+        private System.Windows.Forms.DataGridViewTextBoxColumn First_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Last_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
